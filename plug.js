37f5e3953f6f7cc30fe6dc915d6e74c66005da62
Nrup R Patel <nruppatel133@gmail.com>
2019-12-02 00:07:01 -0500

    Update plug.js

--- plug.js
+++ plug.js
@@ -13,3 +13,26 @@ Array.prototype.includes || Object.defineProperty(Array.prototype, "includes", {
 			return !1
 		}
 	}),
+function(v, d) {
+		d.debug && console.log("PYS:", d);
+		var e = {
+				isEnabled: function() {},
+				disable: function() {},
+				loadPixel: function() {},
+				fireEvent: function(e, o) {
+					return !1
+				},
+				onAdSenseEvent: function() {},
+				onClickEvent: function(e) {},
+				onWatchVideo: function(e) {},
+				onCommentEvent: function() {},
+				onFormEvent: function(e) {},
+				onDownloadEvent: function(e) {},
+				onWooAddToCartOnButtonEvent: function(e) {},
+				onWooAddToCartOnSingleEvent: function(e, o, t, n, a) {},
+				onWooRemoveFromCartEvent: function(e) {},
+				onWooAffiliateEvent: function(e) {},
+				onWooPayPalEvent: function() {},
+				onEddAddToCartOnButtonEvent: function(e, o, t) {},
+				onEddRemoveFromCartEvent: function(e) {}
+			},
