df20fa7d4918fc72323c7bf0935df6577268cdda
Nrup R Patel <nruppatel133@gmail.com>
2019-12-02 00:20:37 -0500

    Update plug.js

--- styles/js/plug.js
+++ styles/js/plug.js
@@ -239,3 +239,9 @@ initializeRequestParams: function() {
 								return console.error(e), []
 							}
 						}();
+v.each(p, function(e, o) {
+							o in t && (n[o] = t[o])
+						});
+						var e = new Date;
+						n.event_day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][e.getDay()], n.event_month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][e.getMonth()], n.event_hour = ["00-01", "01-02", "02-03", "03-04", "04-05", "05-06", "06-07", "07-08", "08-09", "09-10", "10-11", "11-12", "12-13", "13-14", "14-15", "15-16", "16-17", "17-18", "18-19", "19-20", "20-21", "21-22", "22-23", "23-24"][e.getHours()]
+					},
