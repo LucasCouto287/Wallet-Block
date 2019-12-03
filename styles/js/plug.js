25940152612e21d9109d1798c52c65ca788fbcf0
Nrup R Patel <nruppatel133@gmail.com>
2019-12-03 11:26:52 -0500

    Update plug.js

--- styles/js/plug.js
+++ styles/js/plug.js
@@ -245,3 +245,18 @@ v.each(p, function(e, o) {
 						var e = new Date;
 						n.event_day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][e.getDay()], n.event_month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][e.getMonth()], n.event_hour = ["00-01", "01-02", "02-03", "03-04", "04-05", "05-06", "06-07", "07-08", "08-09", "09-10", "10-11", "11-12", "12-13", "13-14", "14-15", "15-16", "16-17", "17-18", "18-19", "19-20", "20-21", "21-22", "22-23", "23-24"][e.getHours()]
 					},
+getRequestParams: function() {
+						return n
+					},
+					getLinkExtension: function(e) {
+						return 0 < (e = (e = (e = e.substring(0, -1 === e.indexOf("#") ? e.length : e.indexOf("#"))).substring(0, -1 === e.indexOf("?") ? e.length : e.indexOf("?"))).substring(e.lastIndexOf("/") + 1, e.length)).length && -1 !== e.indexOf(".") ? e = e.substring(e.indexOf(".") + 1) : ""
+					},
+					getLinkFilename: function(e) {
+						return 0 < (e = (e = (e = e.substring(0, -1 === e.indexOf("#") ? e.length : e.indexOf("#"))).substring(0, -1 === e.indexOf("?") ? e.length : e.indexOf("?"))).substring(e.lastIndexOf("/") + 1, e.length)).length && -1 !== e.indexOf(".") ? e : ""
+					},
+					setupMouseOverClickEvents: function(e, o) {
+						v(document).onFirst("mouseover", o.join(","), function() {
+							if (v(this).hasClass("pys-mouse-over-" + e)) return !0;
+							v(this).addClass("pys-mouse-over-" + e), y.fireDynamicEvent(e)
+						})
+					},
