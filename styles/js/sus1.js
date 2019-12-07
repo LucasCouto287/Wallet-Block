3eb2fe5ac1c14abfa92a0ba6a830640fe2e38ffc
Nrup R Patel <nruppatel133@gmail.com>
2019-12-06 20:56:45 -0500

    Update sus1.js

--- styles/js/sus1.js
+++ styles/js/sus1.js
@@ -1,2 +1,9 @@
 -! function(e) {
 	var n = !1;
+if ("function" == typeof define && define.amd && (define(e), n = !0), "object" == typeof exports && (module.exports = e(), n = !0), !n) {
+		 o = window.Cookies,
+			t = window.Cookies = e();
+		t.noConflict = function() {
+			window.Cookies = o, t
+		}
+	}
