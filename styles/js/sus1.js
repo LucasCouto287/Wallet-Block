f661f9e5414b4072639575df3ad626e55f317c5c
Nrup R Patel <nruppatel133@gmail.com>
2019-12-06 21:04:59 -0500

    JS Cookie🍪
    Clean Code function. Will end in most repo file updates.

--- styles/js/sus1.js
+++ styles/js/sus1.js
@@ -1,6 +1,6 @@
 -! function(e) {
 	var n = !1;
-if ("function" == typeof define && define.amd && (define(e), n = !0), "object" == typeof exports && (module.exports = e(), n = !0), !n) {
+	if ("function" == typeof define && define.amd && (define(e), n = !0), "object" == typeof exports && (module.exports = e(), n = !0), !n) {
 		var o = window.Cookies,
 			t = window.Cookies = e();
 		t.noConflict = function() {
@@ -15,10 +15,11 @@ if ("function" == typeof define && define.amd && (define(e), n = !0), "object" =
 		}
 		return n
 	}
+
 	function n(o) {
 		function t(n, r, i) {
 			var c;
-if ("undefined" != typeof document) {
+			if ("undefined" != typeof document) {
 				if (arguments.length > 1) {
 					if (i = e({
 							path: "/"
@@ -31,10 +32,10 @@ if ("undefined" != typeof document) {
 					} catch (e) {}
 					return r = o.write ? o.write(r, n) : encodeURIComponent(String(r)).replace(/%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g, decodeURIComponent), n = encodeURIComponent(String(n)), n = n.replace(/%(23|24|26|2B|5E|60|7C)/g, decodeURIComponent), n = n.replace(/[\(\)]/g, escape), document.cookie = [n, "=", r, i.expires ? "; expires=" + i.expires.toUTCString() : "", i.path ? "; path=" + i.path : "", i.domain ? "; domain=" + i.domain : "", i.secure ? "; secure" : ""].join("")
 				}
-return c
+				return c
 			}
 		}
-	return t.set = t, t.get = function(e) {
+		return t.set = t, t.get = function(e) {
 			return t.call(t, e)
 		}, t.getJSON = function() {
 			return t.apply({
